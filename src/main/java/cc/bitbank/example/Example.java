f453b3ad32b4058505a2d8281de53ad1bfc7ddff
sinoharamotoki <sinohara@mri.co.jp>
2018-02-25 21:46:24 +0900

    Initial commit

---
+++ src/main/java/cc/bitbank/example/Example.java
@@ -0,0 +1,129 @@
+package cc.bitbank.example;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import cc.bitbank.Bitbankcc;
+import cc.bitbank.entity.Candlestick;
+import cc.bitbank.entity.Depth;
+import cc.bitbank.entity.Ticker;
+import cc.bitbank.entity.Transactions;
+import cc.bitbank.entity.enums.CandleType;
+import cc.bitbank.entity.enums.CurrencyPair;
+import cc.bitbank.exception.BitbankException;
+import cc.bitbank.util.ShowData;
+
+
+/**
+ * Created by tanaka on 2017/04/11.
+ */
+public class Example {
+    public static void main(String args[]) {
+
+    	//propertiesファイル配置ディレクトリ
+        String dir = "/Applications/Eclipse_4.7.2.app/Contents/workspace/java-bitbankcc-master/src/resource";
+        //propertiesファイル名(.propertiesは不要)
+        String source = "example";
+
+
+        try {
+
+            //取得処理
+            URLClassLoader urlLoader = new URLClassLoader(new URL[]{new File(dir).toURI().toURL()});
+            ResourceBundle rb = ResourceBundle.getBundle(source, Locale.getDefault(), urlLoader);
+            Bitbankcc bb = new Bitbankcc();
+            ShowData showData = new ShowData();
+
+            bb.setKey(rb.getString("key"), rb.getString("secret"));
+
+            // ティッカー情報を返す（トップページの大事な情報　前日比較が無いな。。。）
+            Ticker ticker = bb.getTicker(CurrencyPair.BTC_JPY);
+            showData.showTicker(ticker);
+
+            // 板情報を返す（注文に出てくる数字の一覧だと思う）
+            Depth depth = bb.getDepth(CurrencyPair.BTC_JPY);
+            showData.showDepth(depth);
+
+            // 最新の全約定履歴を返す(歩み値だと思う)
+            Transactions.Transaction[] ts = bb.getTransaction(CurrencyPair.BTC_JPY).transactions;
+            showData.showTransaction(ts);
+
+            // 過去分の歩み値(件数多すぎて表示はさせないほうが無難かも)
+            String strYYYYMMDD = "20180223";
+            ts = bb.getTransaction(CurrencyPair.BTC_JPY, strYYYYMMDD).transactions;
+            // showData.showTransaction(ts,strYYYYMMDD);
+
+            List<Candlestick.Ohlcvs.Ohlcv> cs = bb.getCandlestick(CurrencyPair.BTC_JPY, CandleType._1DAY, "2018").candlestick[0].getOhlcvList();
+
+
+
+
+            cs = bb.getCandlestick(CurrencyPair.BTC_JPY, CandleType._1MIN, "20180224").candlestick[0].getOhlcvList();
+
+            for (int i=0;i<cs.size();i++) {
+            		System.out.println(cs.get(i).toString());
+            }
+
+
+
+            //List<Candlestick.Ohlcvs.Ohlcv> cs = bb.getCandlestick(CurrencyPair.BTC_JPY, CandleType._1DAY, "2017").candlestick[0].getOhlcvList();
+
+            //System.out.println(ts2);
+
+
+            /*
+            Assets as = bb.getAsset();
+            System.out.println(as.assets[0]);
+            System.out.println(as.assets[1]);
+            System.out.println(as.assets[2]);
+            System.out.println(as.assets[3]);
+
+            Order order = bb.getOrder(CurrencyPair.BTC_JPY, 90956209);
+            System.out.println(order);
+
+            Order order2 = bb.sendOrder(CurrencyPair.BTC_JPY, BigDecimal.valueOf(10000), BigDecimal.valueOf(0.01), OrderSide.BUY, OrderType.LIMIT);
+            System.out.println(order2);
+
+            Order order3 = bb.cancelOrder(CurrencyPair.BTC_JPY, 129781978);
+            System.out.println(order3);
+
+            long[] ids = {129830841, 129830734};
+            Orders orders = bb.cancelOrders(CurrencyPair.BTC_JPY, ids);
+            System.out.println(orders.orders[0]);
+            System.out.println(orders.orders[1]);
+
+            long[] ids2 = {90956209, 90951996};
+            Orders orders2 = bb.getOrders(CurrencyPair.BTC_JPY, ids2);
+            System.out.println(orders2.orders[0]);
+            System.out.println(orders2.orders[1]);
+
+			Map<String, Long> option = new HashMap<String, Long>();
+            option.put("count", 1L);
+            option.put("since", 1490348550380L);
+            // Option's parameter can be seen https://docs.bitbank.cc/#!/Order/active_orders
+            Orders orders3 = bb.getActiveOrders(CurrencyPair.BTC_JPY, option);
+            for(Order o : orders3.orders) {
+                System.out.println(o);
+            }
+
+            Accounts accounts = bb.getWithdrawalAccounts("btc");
+            for(Accounts.Account a : accounts.accounts) {
+                System.out.println(a);
+            }
+
+            Withdraw w = bb.requestWithdraw("btc", "XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXX",
+                    BigDecimal.valueOf(0.005), "867005", "");
+            System.out.println(w);
+*/
+        } catch (BitbankException e) {
+            System.out.println(e.code);
+        } catch (Exception e) {
+            System.out.println("エラー " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/cc/bitbank/util/ShowData.java
@@ -0,0 +1,76 @@
+package cc.bitbank.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import cc.bitbank.entity.Candlestick;
+import cc.bitbank.entity.Depth;
+import cc.bitbank.entity.Ticker;
+import cc.bitbank.entity.Transactions;
+
+public class ShowData {
+
+	public void showTicker(Ticker ticker) {
+        System.out.println("最新取引価格：" + ticker.last);
+        System.out.println("歩み値の買い注文の最高値：" + ticker.buy);
+        System.out.println("歩み値の売り注文の最安値：" + ticker.sell);
+        System.out.println("安値：" + ticker.low);
+        System.out.println("高値：" + ticker.high);
+        System.out.println("取得日：" + ticker.timestamp);
+        System.out.println("出来高：" + ticker.vol);
+	}
+
+	public void showDepth(Depth depth) {
+		//　多分、注文に表示されている部分だと思う。
+        for(int i=0;i<depth.getAsks().length;i++) {
+            	System.out.println("売り板 i:" + i + "  :" + depth.getAsks()[i][0] + " " + depth.getAsks()[i][1]);
+        }
+
+        for(int i=0;i<depth.getBids().length;i++) {
+            	System.out.println("買い板 i:" + i + "  :" + depth.getBids()[i][0] + " " + depth.getBids()[i][1]);
+        }
+        /*
+        ↑の正式なループ
+        for(int i=0;i<depth.getAsks().length;i++) {
+        		for(int j=0;j<depth.getAsks()[i].length;j++)
+            		// jは0,1しか入らないらしい。。。　固定で出すか
+            		System.out.println("売り板 i:" + i + " j:" + j + "  :" + depth.getAsks()[i][j]);
+        }
+
+        for(int i=0;i<depth.getBids().length;i++) {
+        		for(int j=0;j<depth.getAsks()[i].length;j++)
+        			// jは0,1しか入らないらしい。。。　固定で出すか
+            		System.out.println("買い板 i:" + i + " j:" + j + "  :" + depth.getBids()[i][j]);
+        }*/
+	}
+
+	public void showTransaction(Transactions.Transaction[] ts) {
+        for(int i=0;i<ts.length;i++) {
+    			System.out.println("直近の歩み値：" + ts[i]);
+        }
+	}
+
+	public void showTransaction(Transactions.Transaction[] ts,String strYYYYMMDD) {
+        for(int i=0;i<ts.length;i++) {
+    			System.out.println(strYYYYMMDD + "歩み値：" + ts[i]);
+        }
+	}
+
+	public void showCandlestick(List<Candlestick.Ohlcvs.Ohlcv> cs) {
+        for (int i=0;i<cs.size();i++) {
+	    		System.out.println(cs.get(i).toString());
+	    }
+	}
+
+	public void showCandlestickToday(List<Candlestick.Ohlcvs.Ohlcv> cs,String strYYYYMMDD) throws Exception {
+		String strDate = getYYYYMMDD900000(strYYYYMMDD);
+		SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+        Date date = sdFormat.parse(strDate);
+	}
+
+	public String getYYYYMMDD900000(String strYYYYMMDD) {
+		String strDate = strYYYYMMDD.substring(1, 4) + "/" + strYYYYMMDD.substring(5,2) + "/" + strYYYYMMDD.substring(7,2) + " 09:00:00";
+		return strDate;
+	}
+}
